2b8a6bd5b77dca1d9cc26c4f6abfaa7ddfd3e492
Sudarshan@050 <oraonsudarshan@gmail.com>
2024-06-18 17:02:31 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,5 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-});
+})
 
-//mongodb+srv://Sudarshan:Sudarshan50@cluster0.hasrdjy.mongodb.net/?
